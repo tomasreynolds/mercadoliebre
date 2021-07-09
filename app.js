b86e4b70a93cb45ff1372796bc6b0723edb46ae7
Tomas Reynolds <tomasreynolds@ymail.com>
2021-07-08 23:26:34 -0300

    primera subida

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+const publicPath = path.resolve(__dirname, './public');  
+app.use(express.static(publicPath));
+
+const publicPath2 = path.resolve(__dirname, './views');  
+app.use(express.static(publicPath2));
+
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Corriendo servidor en el puerto 3000");
+});
+
+
+app.get('/', (req,res) =>{
+  res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get('/', (req,res) =>{
+  res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.get('/', (req,res) =>{
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
